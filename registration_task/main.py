37d47c05e48ae83e3146e35d9a11a2be55087d67
Mohammed Arshu PT <arshupt123@gmail.com>
2020-06-03 19:59:12 +0530

    Updated main.py

--- registration_task/main.py
+++ registration_task/main.py
@@ -5,17 +5,35 @@ x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
 y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 """
 
-import numpy
+import numpy as np
 
 class LinearRegression(object):
   """
   An implementation of linear regression model
   """
+  def __init__(self):
+    self.lr = 0.001
+    self.W = None
+    self.b = None
   
-  def fit(_input, _output):
-    pass
+  def fit(self,X,y):
+    X = np.array(X)
+    m,train_X_shape = X.shape
+    self.W = np.zeros(train_X_shape)
+    self.b = 0
+
+    for _ in range(10000):
+      y_pred = np.dot(X, self.W) + self.b
+      dw = (1 / m) * np.dot(X.T, (y_pred - y))
+      db = (1 / m) * np.sum(y_pred - y)
+      self.W =self.W - self.lr * dw
+      self.b =self.b - self.lr * db
     
-  def predict(_input):
-    pass
+  def predict(self,X):
+    y_pred = np.floor((np.dot(X,self.W)+self.b))
+    Y = []
+    for x in y_pred:
+      Y.append(int(x))
+    return Y
     
     
