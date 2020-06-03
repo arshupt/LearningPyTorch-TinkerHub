602c551968ca510503063bacffc6b546edb9c870
Mohammed Arshu PT <arshupt123@gmail.com>
2020-06-03 20:06:48 +0530

    Updates

--- registration_task/main.py
+++ registration_task/main.py
@@ -13,27 +13,27 @@ class LinearRegression(object):
   """
   def __init__(self):
     self.lr = 0.001
-    self.W = None
+    self.w = None
     self.b = None
   
-  def fit(self,X,y):
-    X = np.array(X)
-    m,train_X_shape = X.shape
-    self.W = np.zeros(train_X_shape)
+  def fit(self,x,y):
+    x = np.array(x)
+    m,train_x_shape = x.shape
+    self.w = np.zeros(train_x_shape)
     self.b = 0
 
-    for _ in range(10000):
-      y_pred = np.dot(X, self.W) + self.b
-      dw = (1 / m) * np.dot(X.T, (y_pred - y))
+    for i in range(10000):
+      y_pred = np.dot(x, self.w) + self.b
+      dw = (1 / m) * np.dot(x.T, (y_pred - y))
       db = (1 / m) * np.sum(y_pred - y)
-      self.W =self.W - self.lr * dw
+      self.w =self.w - self.lr * dw
       self.b =self.b - self.lr * db
     
-  def predict(self,X):
-    y_pred = np.floor((np.dot(X,self.W)+self.b))
+  def predict(self,x):
+    y_pred = np.floor((np.dot(x,self.w)+self.b))
     Y = []
-    for x in y_pred:
-      Y.append(int(x))
+    for j in y_pred:
+      Y.append(int(j))
     return Y
     
     
